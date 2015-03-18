7ab53e1a639b165ae722dc411ec0869cceaaea73
jiaping <jiaping1818@qq.com>
2015-03-18 22:21:18 +0800

    first

---
+++ Sample1/Sample1.Game/Sample1Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Paradox;
+using SiliconStudio.Paradox.Effects;
+
+namespace Sample1
+{
+    public class Sample1Game:Game
+    {
+        //protected override async Task LoadContent()
+        //{
+        //    //await base.LoadContent();
+
+        //    //RenderSystem.Pipeline.Renderers.Add(new BackgroundRenderer(Services, "ParadoxBackground"));
+        //}
+    }
+}
---
+++ Sample1/Sample1.Windows/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Sample1Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
